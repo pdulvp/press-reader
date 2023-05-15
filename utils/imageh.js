564ad1427d70cceecc957d552844552e35afc2bf
pdulvp <pdulvp@laposte.net>
2023-05-15 14:00:50 +0200

    Add utils/imageh to generate thumbnails

---
+++ utils/imageh.js
@@ -0,0 +1,40 @@
+var PImage = require('pureimage');
+const { Readable } = require('stream');
+const { Writable } = require('stream');
+
+var fs = require('fs');
+const { buffer } = require('stream/consumers');
+
+var imageh = {
+    resize: (imageData, width, height) => {
+        return new Promise(function(resolve, reject) {
+            const stream = Readable.from(Buffer.from(imageData, "binary"));
+            PImage.decodePNGFromStream(stream).then((img) => {
+                console.log("size is",img.width,img.height);
+                if (height == null) height = parseInt(width * img.height / img.width);
+                var img2 = PImage.make(width,height);
+                var c = img2.getContext('2d');
+                c.drawImage(img,
+                    0, 0, img.width, img.height, // source dimensions
+                    0, 0, width, height                 // destination dimensions
+                );
+                let body = [];
+                const myWritable = new Writable({
+                    write(chunk, encoding, callback) {
+                        body.push(chunk);
+                        callback();
+                    },
+                    writev(chunks, callback) {
+                        chunks.forEach(c => body.push(c));
+                        callback();
+                    },
+                  });
+                PImage.encodePNGToStream(img2, myWritable).then(() => {
+                    body = Buffer.concat(body).toString("binary");
+                    resolve(body);
+                });
+            });
+        });
+    }
+}
+module.exports = imageh;
